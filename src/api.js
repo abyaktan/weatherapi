41492ffb59170938b53cae5ae53e795f8820fc9f
Abyakta <triharyanto@Abyakta-2.local>
2022-11-04 11:18:34 +0700

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "c48bce4565msh205fba302179d36p115635jsn8db458dd2c49",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "15f5a4621e4ec997ba13a910faca7ec2"; // enter your key from openweather API
+  
---
+++ src/hooks/currentWeatherFetch.js
@@ -0,0 +1,20 @@
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+
+
+const currentWeatherFetch = fetch(
+    `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+  );
+  const forecastFetch = fetch(
+    `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+  );
+
+  Promise.all([currentWeatherFetch, forecastFetch])
+    .then(async (response) => {
+      const weatherResponse = await response[0].json();
+      const forcastResponse = await response[1].json();
+
+      setCurrentWeather({ city: searchData.label, ...weatherResponse });
+      setForecast({ city: searchData.label, ...forcastResponse });
+    })
+    .catch(console.log);
+
